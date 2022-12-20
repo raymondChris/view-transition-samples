a765b85a663cd188c0fc78f37487653b365c0185
Raymond Codinera <raymond.codinera@spindox.it>
2022-12-20 20:51:20 +0100

    ✨ Expanding Image

--- src/script.js
+++ src/script.js
@@ -1,24 +1,53 @@
-import { getPageContent, onLinkNavigate } from "../utils.js";
+import {
+  getPageContent,
+  onLinkNavigate,
+  transitionHelper,
+  getLink,
+} from "./utils.js";
 
-onLinkNavigate(async ({ toPath }) => {
-  const content = await getPageContent(toPath);
+const galleryPath = "/";
+const catsPath = `${galleryPath}cats/`;
 
-  startViewTransition(() => {
-    // This is a pretty heavy-handed way to update page content.
-    // In production, you'd likely be modifying DOM elements directly,
-    // or using a framework.
-    // innerHTML is used here just to keep the DOM update super simple.
-    document.body.innerHTML = content;
-  });
-});
-
-// A little helper function like this is really handy
-// to handle progressive enhancement.
-function startViewTransition(callback) {
-  if (!document.startViewTransition) {
-    callback();
-    return;
+function getNavigationType(fromPath, toPath) {
+  if (fromPath.startsWith(catsPath) && toPath === galleryPath) {
+    return "cat-page-to-gallery";
   }
 
-  document.startViewTransition(callback);
+  if (fromPath === galleryPath && toPath.startsWith(catsPath)) {
+    return "gallery-to-cat-page";
+  }
+
+  return "other";
 }
+
+onLinkNavigate(async ({ fromPath, toPath }) => {
+  const navigationType = getNavigationType(fromPath, toPath);
+  const content = await getPageContent(toPath);
+
+  let targetThumbnail;
+
+  if (navigationType === "gallery-to-cat-page") {
+    targetThumbnail = getLink(toPath).querySelector("img");
+    targetThumbnail.style.viewTransitionName = "banner-img";
+  }
+
+  const transition = transitionHelper({
+    updateDOM() {
+      // This is a pretty heavy-handed way to update page content.
+      // In production, you'd likely be modifying DOM elements directly,
+      // or using a framework.
+      // innerHTML is used here just to keep the DOM update super simple.
+      document.body.innerHTML = content;
+
+      if (navigationType === "cat-page-to-gallery") {
+        targetThumbnail = getLink(fromPath).querySelector("img");
+        targetThumbnail.style.viewTransitionName = "banner-img";
+      }
+    },
+  });
+
+  transition.finished.finally(() => {
+    // Clear the temporary tag
+    if (targetThumbnail) targetThumbnail.style.viewTransitionName = "";
+  });
+});
